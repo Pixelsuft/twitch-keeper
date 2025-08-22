1600c196710fc5be0b560652c2fa024a9d0a8301
Pixelsuft <yuskivl07@mail.ru>
2025-08-22 22:09:31 +0700

    styles

--- main.py
+++ main.py
@@ -6,8 +6,9 @@ if 1 and os.getenv('PYCHARM_HOSTED') and int(os.environ['PYCHARM_HOSTED']):
         if not os.path.isfile(f'ui_{i}.py') or os.path.getmtime(f'ui_{i}.py') < os.path.getmtime(f'ui/{i}.ui'):
             print(f'Rebuilding ui_{i}.py')
             subprocess.call(['pyuic6', '-o', f'ui_{i}.py', f'ui/{i}.ui'])
-from PyQt6 import QtWidgets, QtCore
+from PyQt6 import QtWidgets
 from theming import Theming
+from styling import Styling
 from ui_main import Ui_MainWindow
 
 
@@ -15,14 +16,17 @@ class App:
     def __init__(self, args: list):
         self.exit_code = 0
         self.app = QtWidgets.QApplication(args)
+        self.cwd = os.path.dirname(__file__) or os.getcwd()
         self.theming = Theming()
+        self.styling = Styling()
         self.main_win = QtWidgets.QMainWindow()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self.main_win)
         self.main_win.setFixedSize(self.main_win.size())
-        self.main_win.setWindowFlag(QtCore.Qt.WindowType.WindowMaximizeButtonHint, False)
         self.theming.init_on_window(self.main_win)
         self.dark = self.theming.is_dark()
+        self.styling.read_styles(os.path.join(self.cwd, 'styles'), not self.dark, self.dark)
+        self.styling.apply_on_win(self.main_win, self.dark)
 
     def run(self) -> None:
         self.main_win.show()
---
+++ styling.py
@@ -0,0 +1,20 @@
+import os
+
+
+class Styling:
+    def __init__(self) -> None:
+        self.style = ''
+        self.style_light = ''
+        self.style_dark = ''
+
+    def read_styles(self, path: str, read_light: bool = True, read_dark: bool = True) -> None:
+        self.style = open(os.path.join(path, 'style.qss'), 'r', encoding='utf-8').read()
+        if not self.style.endswith('\n'):
+            self.style += '\n'
+        if read_light:
+            self.style_light = open(os.path.join(path, 'light.qss'), 'r', encoding='utf-8').read()
+        if read_dark:
+            self.style_dark = open(os.path.join(path, 'dark.qss'), 'r', encoding='utf-8').read()
+
+    def apply_on_win(self, win, is_dark: bool) -> None:
+        win.setStyleSheet(self.style + (self.style_dark if is_dark else self.style_light))
